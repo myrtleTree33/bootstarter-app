1bd01e841c3656bad4ef94de32a961047658b587
joel_tong <ec2-user@ip-172-31-4-103.ap-southeast-1.compute.internal>
2018-05-03 09:56:07 +0000

    doing some tidying upo

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import rootService from '../services';
-import { Link } from 'react-router-dom';
 
 
 class Login extends Component {
