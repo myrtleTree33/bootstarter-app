df7ab8fb48c975b5d8c07ebe05d40d6cab1146c6
jhtong <me at joeltong dot org>
2018-05-11 00:02:12 +0800

    restructured code with spacing

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import uuidv1 from "uuid/v1";
+
 import * as articleActions from "../actions";
 
 class Form extends Component {
