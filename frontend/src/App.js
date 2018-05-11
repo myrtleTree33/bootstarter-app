107f564d87d50fea1a0394a727bde7f42b782417
joel tong <ec2-user@ip-172-31-5-147.ap-southeast-1.compute.internal>
2018-05-11 07:00:47 +0000

    further refactored components and the way they are written

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import JwtAuth from "./components/JwtAuth";
 import Protected from "./components/Protected";
-import userService from "./services/userService";
+import userService from "./services/user";
 
 /**
  * Allows the creation of a protected route, if the user is not signed in.
@@ -27,7 +26,17 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 };
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.bootstrapUser = this.bootstrapUser.bind(this);
+  }
+  
   componentDidMount() {
+    this.bootstrapUser();
+  }
+  
+  bootstrapUser() {
     userService
       .getUser()
       .then(res => {
@@ -36,13 +45,11 @@ class App extends Component {
       })
       .catch(err => console.error(`Unable to connect to backend API=${err}`));
   }
-
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
           <nav>
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
@@ -52,7 +59,6 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
-          <Route path="/user/jwt" component={JwtAuth} />
           <PrivateRoute path="/protected" component={Protected} />
         </Switch>
       </div>
